fdca4fdd22153623f83f76c175e83dcd00943479
Klara Bragesjö Björkén <klara.bjorken@gmail.com>
2023-08-20 01:54:25 +0200

    Saving the project on GitHub

--- Estée Lauder App/ContentView.swift
+++ Estée Lauder App/ContentView.swift
@@ -18,6 +18,7 @@ struct ContentView: View {
 
     var body: some View {
         NavigationView {
+            
             List {
                 ForEach(items) { item in
                     NavigationLink {
